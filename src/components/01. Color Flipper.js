19d49a84bcb9a2e41fe4b59c5230be7dd324b2a4
JuHeyong <wkdwngud963@gmail.com>
2025-01-14 18:44:19 +0900

    color flipper

--- src/components/01. Color Flipper.js
+++ src/components/01. Color Flipper.js
@@ -6,18 +6,16 @@ function ColorFlipper() {
     const changeColor =
         `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 
-    console.log(changeColor);
-
     const [Color, setColor] = useState();
 
     return (
-        <div className="colorflipper" style={{backgroundColor: Color}}>
-            <div className="header">
-                <h1 className="left">Color Flipper</h1>
-                <h1 className="right"> Simple Hex</h1>
+        <div id="colorflipper" style={{backgroundColor: Color}}>
+            <div id="header">
+                <h1 id="left">Color Flipper</h1>
+                <h1 id="right"> Simple Hex</h1>
             </div>
-            <h1 className="colorName">Background Color: {Color}</h1>
-            <button className="button" style={{backgroundColor: Color}} onClick={() => {
+            <h1 id="colorName">Background Color: {Color}</h1>
+            <button id="button" style={{backgroundColor: Color}} onClick={() => {
                 setColor(changeColor)}}> CLICK ME </button>
         </div>
     )
